b9d00315ebb6cfa4aa2ce76ab2773c95fbccf91a
Kadir <kadir-metin@outlook.com.tr>
2023-05-21 00:06:52 +0300

    Initialize app

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Anasayfa | Softforware</title>
+        <meta name="description" content="Blog for developers" />
+        <link rel="icon" href="/softforware.png" />
+      </Head>
+      <main>
+        <div>HELLO WORLD</div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
