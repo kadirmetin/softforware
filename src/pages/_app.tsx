14079721b34eb8f147647e3c152e0e72fe8e8c30
Kadir <kadir-metin@outlook.com.tr>
2023-07-31 21:20:37 +0300

    feat: Added Carousel, PostList, Footer, and Header components with API integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,26 @@
 import { type Session } from "next-auth";
+import { Roboto } from "next/font/google";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+  style: ["italic", "normal"],
+});
+
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -10,7 +28,12 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <main className={roboto.className}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </main>
     </SessionProvider>
   );
 };
