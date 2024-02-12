f87d9f80b44645adb5c0ac910b5dcbb476ee8724
Kadir <kadir-metin@outlook.com.tr>
2024-02-12 23:10:07 +0300

    Add @next/third-parties

--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -1,6 +1,6 @@
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 import { Analytics } from "@vercel/analytics/react";
 import Head from "next/head";
-import Script from "next/script";
 import React from "react";
 import Footer from "~/components/Footer/Footer";
 import Header from "~/components/Header/Header";
@@ -17,21 +17,10 @@ export default function RootLayout({
         <meta name="description" content="Blog for developers" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
-        `}
-      </Script>
       <Header />
       {children}
+      <GoogleTagManager gtmId={`${process.env.NEXT_PUBLIC_GT_ID}`} />
+      <GoogleAnalytics gaId={`${process.env.NEXT_PUBLIC_GA_ID}`} />
       <Analytics />
       <Footer />
     </>
