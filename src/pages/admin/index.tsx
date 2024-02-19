6cf48df84b68cbb9b1011294f51a2ed44eb478b7
Kadir <kadir-metin@outlook.com.tr>
2024-02-19 16:52:30 +0300

    v1.2 live

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -3,9 +3,9 @@ import { Box, Typography } from "@mui/material";
 const AdminPage: React.FC = () => {
   return (
     <Box className="flex flex-col p-5">
-      <Typography>Admin panele hoşgeldin, v.1.1 yayınlandı</Typography>
-      <Typography>- Editor düzenlendi.</Typography>
-      <Typography>- Editörün birkaç ufak hatası giderildi.</Typography>
+      <Typography>Admin panele hoşgeldin, v.1.2 yayınlandı</Typography>
+      <Typography>- Post listelemesi eklendi</Typography>
+      <Typography>- Post düzenleme ve silme eklendi.</Typography>
     </Box>
   );
 };
